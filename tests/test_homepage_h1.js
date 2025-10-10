d3e8ffe7e964a033920af11cf96d75597169ee6e
Drew <drew17201@gmail.com>
2025-10-10 10:41:37 -0400

    added tailwind and a new test

--- tests/test_homepage_h1.js
+++ tests/test_homepage_h1.js
@@ -1,16 +1,24 @@
 const { test, expect } = require("@playwright/test");
 
-test("page loads and has an H1", async ({ page }, testInfo) => {
+test("page loads and has an H1, header, main, and nav", async ({ page }, testInfo) => {
     await page.goto("/");
 
     const h1 = page.locator("h1");
+    const header = page.locator("header");
+    const nav = page.locator("nav");
+    const main = page.locator("main");
     await expect(h1).toBeVisible;
+    await expect(header).toBeVisible;
+    await expect(nav).toBeVisible;
+    await expect(main).toBeVisible;
 
 
     //single screenshot
     await page.screenshot(
         {
-        path: testInfo.outputPath("homepage_h1.png"),
+        path: testInfo.outputPath("homepage_HHNM.png"),
         fullPage: true
         });
+
+    
 });
